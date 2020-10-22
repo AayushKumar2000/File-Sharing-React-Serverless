9519ce65e3d94998e5961743a8917e04c27d70c4
Aayush Kumar <you@example.com>
2020-10-23 00:10:43 +0530

    create s3 bucket life-cycle rule for object expiration

--- serverless-terraform/s3-public/s3-public.tf
+++ serverless-terraform/s3-public/s3-public.tf
@@ -13,6 +13,73 @@ resource "aws_s3_bucket" "s3_bucket" {
      expose_headers  = ["ETag"]
      max_age_seconds = 3000
    }
+
+
+   lifecycle_rule {
+     id      = "expire-1d"
+     enabled = true
+
+     tags = {
+       "expireValue" = "1day"
+     }
+
+     expiration {
+       days = 1
+     }
+   }
+
+   lifecycle_rule {
+     id      = "expire-2d"
+     enabled = true
+
+     tags ={
+       "expireValue" = "2day"
+     }
+
+     expiration {
+       days = 2
+     }
+   }
+
+   lifecycle_rule {
+     id      = "expire-3d"
+     enabled = true
+
+     tags ={
+       "expireValue" = "3day"
+     }
+
+     expiration {
+       days = 3
+     }
+   }
+
+   lifecycle_rule {
+     id      = "expire-4d"
+     enabled = true
+
+     tags ={
+       "expireValue" = "4day"
+     }
+
+     expiration {
+       days = 4
+     }
+   }
+
+   lifecycle_rule {
+     id      = "expire-7d"
+     enabled = true
+
+     tags= {
+       "expireValue" = "7day"
+     }
+
+     expiration {
+       days = 7
+     }
+   }
+
 }
 
 
